b6af33901c55c470080d1643ef7caf0a4e2d1296
Aidan Law <afl@aidan-law.com>
2016-04-17 19:04:04 -0500

    Added a Log

--- PowerFailure.sh
+++ PowerFailure.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-php /home/pi/BatteryAlert.php;
-ssh root@10.1.1.4 'sh /vmfs/volumes/datastore1/scripts/ESXIShutdown.sh';
-ssh -p 1022 root@10.1.1.1 'shutdown -r now';
+touch /home/pi/shutdown.log;
+php /home/pi/BatteryAlert.php >> /home/pi/shutdown.log;
+ssh root@10.1.1.4 'sh /vmfs/volumes/datastore1/scripts/ESXIShutdown.sh' >> /home/pi/shutdown.log;
+ssh -p 1022 root@10.1.1.1 'shutdown -r now' >> /home/pi/shutdown.log;
 shutdown 0;
